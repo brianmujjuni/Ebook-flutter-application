d5db23cc3f576c4600ba8b3841c0cd7b9c46cd03
brianmujjuni <brianmujjuni@engineer.com>
2025-07-12 00:27:06 +0300

    create rounded button widget

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -15,21 +15,79 @@ class WelcomeScreen extends StatelessWidget {
               ),
               fit: BoxFit.fill),
         ),
-       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          RichText(text: TextSpan(
-            children: [
-              TextSpan(
-                text: "Flamin",
-                style: Theme.of(context).textTheme.displayMedium
-              )
-            ]
-          ))
-        ],
-       ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RichText(
+              text: TextSpan(
+                  style: Theme.of(context).textTheme.displayMedium,
+                  children: [
+                    TextSpan(
+                      text: "Flamin",
+                    ),
+                    TextSpan(
+                      text: "go",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ]),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              child: RoundedButton(
+                text: "start reading",
+                fontSize: 20,
+                press: () {},
+              
+              ),
+            )
+          ],
+        ),
       ),
-      
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback press;
+  final double verticalPadding;
+  final double fontSize;
+
+  const RoundedButton({
+    super.key,
+    required this.text,
+    required this.press,
+    this.verticalPadding = 16,
+    this.fontSize = 16,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(vertical: verticalPadding),
+        padding: EdgeInsets.symmetric(vertical: 18, horizontal: 30),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 13),
+              blurRadius: 30,
+              color: Color(0xFF666666).withValues(),
+            )
+          ],
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
