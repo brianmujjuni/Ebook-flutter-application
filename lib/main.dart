840c7b6ccd884375224e16c30ce9b3ed4a10a908
brianmujjuni <brianmujjuni@engineer.com>
2025-07-14 00:16:13 +0300

    create reuseable rounded button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ebook_reading_app/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +16,9 @@ class MyApp extends StatelessWidget {
         title: 'Book App',
         theme: ThemeData(
           scaffoldBackgroundColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.apply(
+                displayColor: kBlackColor,
+              ),
           useMaterial3: true,
         ),
         home: WelcomeScreen());
@@ -35,6 +39,78 @@ class WelcomeScreen extends StatelessWidget {
                 "assets/images/Bitmap.png",
               ),
               fit: BoxFit.fill),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.displaySmall,
+                children: [
+                  TextSpan(
+                    text: "Flamin",
+                  ),
+                  TextSpan(
+                    text: "go",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+                child: RoundedButton(
+              text: "Start reading",
+              fontSize: 20,
+              press: () {},
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback press;
+  final double verticlePadding;
+  final double fontSize;
+
+  const RoundedButton({
+    super.key,
+    required this.text,
+    required this.press,
+    this.verticlePadding = 16,
+    this.fontSize = 16,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(vertical: 16),
+        padding:
+            EdgeInsets.symmetric(vertical: verticlePadding, horizontal: 30),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 13),
+              blurRadius: 30,
+              color: Color(0xFF666666).withValues(),
+            )
+          ],
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
