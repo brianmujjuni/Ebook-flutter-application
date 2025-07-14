ddc81dc433628dfb4aaa852066e3e4f00ac51dec
brianmujjuni <brianmujjuni@engineer.com>
2025-07-14 13:20:32 +0300

    refactor product container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -41,107 +41,136 @@ class HomeScreen extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            Container(
-              margin: EdgeInsets.only(left: 24,bottom: 40),
-              height: 245,
-              width: 202,
-              // color: Colors.amber,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      height: 221,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(29),
-                        boxShadow: [
-                          BoxShadow(
-                              offset: Offset(0, 30),
-                              blurRadius: 33,
-                              color: kShadowColor)
-                        ],
-                      ),
-                    ),
-                  ),
-                  Image.asset(
-                    "assets/images/book-1.png",
-                    width: 150,
-                  ),
-                  Positioned(
-                    top: 35,
-                    right: 10,
-                    child: Column(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.favorite_border),
-                        ),
-                        BookRating(
-                          score: 4.9,
-                        )
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    top: 160,
-                    child: Container(
-                      height: 85,
-                      width: 202,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 24),
-                            child: RichText(
-                              maxLines: 2,
-                              text: TextSpan(
-                                style: TextStyle(color: kBlackColor),
-                                children: [
-                                  TextSpan(
-                                    text: "Crushing & Influence\n ",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  TextSpan(
-                                      text: "Automex Technologies",
-                                      style: TextStyle(color: kBlackColor))
-                                ],
-                              ),
-                            ),
-                          ),
-                          Spacer(),
-                          Row(
-                            children: [
-                              GestureDetector(
-                                onTap: () {},
-                                child: Container(
-                                  width: 101,
-                                  padding: EdgeInsets.symmetric(vertical: 10),
-                                  alignment: Alignment.center,
-                                  child: Text("Details"),
-                                ),
-                              ),
-                              Expanded(
-                                child: TwoRoundedButton(
-                                  text: "Read",
-                                  press: () {},
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
+            ReadingListCard(
+              image: "assets/images/book-1.png",
+              title: "Crushing & Influence",
+              auth: "Automex technologies",
+              rating: 4.9,
+              pressDetails: (){},
+              pressRead: (){},
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ReadingListCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String auth;
+  final double rating;
+  final VoidCallback pressDetails;
+  final VoidCallback pressRead;
+  const ReadingListCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.auth,
+    required this.rating,
+    required this.pressDetails,
+    required this.pressRead,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 24, bottom: 40),
+      height: 245,
+      width: 202,
+      // color: Colors.amber,
+      child: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            bottom: 0,
+            right: 0,
+            child: Container(
+              height: 221,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 30),
+                      blurRadius: 33,
+                      color: kShadowColor)
+                ],
+              ),
+            ),
+          ),
+          Image.asset(
+            image,
+            width: 150,
+          ),
+          Positioned(
+            top: 35,
+            right: 10,
+            child: Column(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.favorite_border),
+                ),
+                BookRating(
+                  score: rating,
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 160,
+            child: Container(
+              height: 85,
+              width: 202,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: RichText(
+                      maxLines: 2,
+                      text: TextSpan(
+                        style: TextStyle(color: kBlackColor),
+                        children: [
+                          TextSpan(
+                            text: "$title \n ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                              text: auth, style: TextStyle(color: kBlackColor))
+                        ],
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: pressDetails,
+                        child: Container(
+                          width: 101,
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.center,
+                          child: Text("Details"),
+                        ),
+                      ),
+                      Expanded(
+                        child: TwoRoundedButton(
+                          text: "Read",
+                          press: pressRead,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
