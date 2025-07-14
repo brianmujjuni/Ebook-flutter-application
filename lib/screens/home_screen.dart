6cf9ae2e3d48e84be41c9c4c148180293e0e8e3e
brianmujjuni <brianmujjuni@engineer.com>
2025-07-14 14:46:11 +0300

    create reading card list widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ebook_reading_app/constants.dart';
 import 'package:ebook_reading_app/widgets/book_rating.dart';
+import 'package:ebook_reading_app/widgets/reading_card_list.dart';
 import 'package:ebook_reading_app/widgets/two_rounded_button.dart';
 import 'package:flutter/material.dart';
 
@@ -41,136 +42,32 @@ class HomeScreen extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            ReadingListCard(
-              image: "assets/images/book-1.png",
-              title: "Crushing & Influence",
-              auth: "Automex technologies",
-              rating: 4.9,
-              pressDetails: (){},
-              pressRead: (){},
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ReadingListCard(
+                    image: "assets/images/book-1.png",
+                    title: "Crushing & Influence",
+                    auth: "Automex technologies",
+                    rating: 4.9,
+                    pressDetails: (){},
+                    pressRead: (){},
+                  ),
+                    ReadingListCard(
+                    image: "assets/images/book-2.png",
+                    title: "Top Ten Business Hacks",
+                    auth: "Automex technologies",
+                    rating: 4.8,
+                    pressDetails: (){},
+                    pressRead: (){},
+                  ),
+                  SizedBox(width: 30,)
+                ],
+              ),
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ReadingListCard extends StatelessWidget {
-  final String image;
-  final String title;
-  final String auth;
-  final double rating;
-  final VoidCallback pressDetails;
-  final VoidCallback pressRead;
-  const ReadingListCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.auth,
-    required this.rating,
-    required this.pressDetails,
-    required this.pressRead,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 24, bottom: 40),
-      height: 245,
-      width: 202,
-      // color: Colors.amber,
-      child: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            bottom: 0,
-            right: 0,
-            child: Container(
-              height: 221,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(29),
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(0, 30),
-                      blurRadius: 33,
-                      color: kShadowColor)
-                ],
-              ),
-            ),
-          ),
-          Image.asset(
-            image,
-            width: 150,
-          ),
-          Positioned(
-            top: 35,
-            right: 10,
-            child: Column(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.favorite_border),
-                ),
-                BookRating(
-                  score: rating,
-                )
-              ],
-            ),
-          ),
-          Positioned(
-            top: 160,
-            child: Container(
-              height: 85,
-              width: 202,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 24),
-                    child: RichText(
-                      maxLines: 2,
-                      text: TextSpan(
-                        style: TextStyle(color: kBlackColor),
-                        children: [
-                          TextSpan(
-                            text: "$title \n ",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          TextSpan(
-                              text: auth, style: TextStyle(color: kBlackColor))
-                        ],
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: pressDetails,
-                        child: Container(
-                          width: 101,
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          alignment: Alignment.center,
-                          child: Text("Details"),
-                        ),
-                      ),
-                      Expanded(
-                        child: TwoRoundedButton(
-                          text: "Read",
-                          press: pressRead,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
       ),
     );
   }
