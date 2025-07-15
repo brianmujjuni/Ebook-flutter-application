9af98b70ad212aac5165506a3508f8c064e159cb
brianmujjuni <brianmujjuni@engineer.com>
2025-07-15 15:00:50 +0300

    finalize home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,63 +11,277 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/main_page_bg.png"),
-              alignment: Alignment.topCenter,
-              fit: BoxFit.fitWidth),
-        ),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: size.height * .1,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: RichText(
-                text: TextSpan(
-                  style: Theme.of(context).textTheme.displaySmall,
-                  children: [
-                    TextSpan(text: "What are you \nreading "),
-                    TextSpan(
-                        text: "today?",
-                        style: TextStyle(fontWeight: FontWeight.bold))
-                  ],
-                ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/main_page_bg.png"),
+                    alignment: Alignment.topCenter,
+                    fit: BoxFit.fitWidth),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ReadingListCard(
-                    image: "assets/images/book-1.png",
-                    title: "Crushing & Influence",
-                    auth: "Automex technologies",
-                    rating: 4.9,
-                    pressDetails: (){},
-                    pressRead: (){},
+                  SizedBox(
+                    height: size.height * .1,
                   ),
-                    ReadingListCard(
-                    image: "assets/images/book-2.png",
-                    title: "Top Ten Business Hacks",
-                    auth: "Automex technologies",
-                    rating: 4.8,
-                    pressDetails: (){},
-                    pressRead: (){},
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: RichText(
+                      text: TextSpan(
+                        style: Theme.of(context).textTheme.displaySmall,
+                        children: [
+                          TextSpan(text: "What are you \nreading "),
+                          TextSpan(
+                              text: "today?",
+                              style: TextStyle(fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ),
                   ),
-                  SizedBox(width: 30,)
+                  SizedBox(
+                    height: 30,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        ReadingListCard(
+                          image: "assets/images/book-1.png",
+                          title: "Crushing & Influence",
+                          auth: "Automex technologies",
+                          rating: 4.9,
+                          pressDetails: () {},
+                          pressRead: () {},
+                        ),
+                        ReadingListCard(
+                          image: "assets/images/book-2.png",
+                          title: "Top Ten Business Hacks",
+                          auth: "Automex technologies",
+                          rating: 4.8,
+                          pressDetails: () {},
+                          pressRead: () {},
+                        ),
+                        SizedBox(
+                          width: 30,
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.displaySmall,
+                            children: [
+                              TextSpan(text: "Best of the "),
+                              TextSpan(
+                                text: "day",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                        bestOfTheDayCard(size, context),
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.displaySmall,
+                            children: [
+                              TextSpan(text: "Continue "),
+                              TextSpan(
+                                text: "reading...",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 80,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(38.5),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 30),
+                                blurRadius: 33,
+                                color: Color(0xFFD3D3D3).withOpacity(.84),
+                              )
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(38.5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 20, right: 20),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Crushing & Influence",
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Text(
+                                                "Automex Technologies",
+                                                style: TextStyle(
+                                                  color: kLightBlackColor,
+                                                ),
+                                              ),
+                                              Align(
+                                                alignment:
+                                                    Alignment.bottomRight,
+                                                child: Text(
+                                                  "Chapter 7 of 10",
+                                                  style: TextStyle(
+                                                      fontSize: 10,
+                                                      color: kLightBlackColor),
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                height: 5,
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Image.asset(
+                                          "assets/images/book-1.png",
+                                          width: 55,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 7,
+                                  width: size.width * .65,
+                                  decoration: BoxDecoration(
+                                    color: kProgressIndicator,
+                                    borderRadius: BorderRadius.circular(7),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container bestOfTheDayCard(Size size, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      width: double.infinity,
+      height: 205,
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0, // Added to stretch width properly
+            child: Container(
+              padding:
+                  EdgeInsets.only(left: 24, top: 24, right: size.width * .35),
+              height: 185,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFEAEAEA).withOpacity(0.45),
+                borderRadius: BorderRadius.circular(29),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "New York Time Best For 11th March 2025",
+                    style: TextStyle(fontSize: 9, color: kLightBlackColor),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "How To Win \nFriends & Influence",
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  Text(
+                    "Automex Technologies",
+                    style: TextStyle(color: kLightBlackColor),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      BookRating(score: 4.9),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "When the earth was flat and everyone wanted to win the game of the best and people...",
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: kLightBlackColor,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Image.asset(
+              "assets/images/book-3.png",
+              width: size.width * .37,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SizedBox(
+              height: 40,
+              width: size.width * .3,
+              child: TwoRoundedButton(text: "Read", press: () {}),
+            ),
+          )
+        ],
       ),
     );
   }
