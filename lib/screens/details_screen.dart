35d8b5858f3cc62472c71c06b05a008bd77228f6
brianmujjuni <brianmujjuni@engineer.com>
2025-07-16 20:14:02 +0300

    create chapterCard widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,3 +1,6 @@
+import 'package:ebook_reading_app/constants.dart';
+import 'package:ebook_reading_app/widgets/book_rating.dart';
+import 'package:ebook_reading_app/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -10,46 +13,209 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Stack(alignment: Alignment.topCenter, children: [
-            Container(
-              height: size.height * .4,
-              decoration: BoxDecoration(
+          Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Container(
+                height: size.height * .4,
+                decoration: BoxDecoration(
                   image: DecorationImage(
                     image: AssetImage("assets/images/bg.png"),
                     fit: BoxFit.fitWidth,
                   ),
                   borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(50),
-                      bottomRight: Radius.circular(50))),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: size.height * .1,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Crushing &",style: Theme.of(context).textTheme.headlineMedium,),
-                            Text("Influence",style: Theme.of(context).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold),)
-                          ],
-                        )),
-                        Image.asset(
-                          "assets/images/book-1.png",
-                          height: 200,
-                        )
-                      ],
-                    )
-                  ],
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: size.height * .1,
+                      ),
+                      BookInfo()
+                    ],
+                  ),
                 ),
               ),
-            )
-          ])
+              Padding(
+                padding: EdgeInsets.only(top: size.height * .4 - 30),
+                child: Column(
+                  children: [
+                    ChapterCard(
+                      name: "Money",
+                      chapterNumber: 1,
+                      tag: "Life is about change",
+                      press: () {},
+                    ),
+                    ChapterCard(
+                      name: "Power",
+                      chapterNumber: 1,
+                      tag: "Everything loves. power",
+                      press: () {},
+                    ),
+                    ChapterCard(
+                      name: "Influence",
+                      chapterNumber: 1,
+                      tag: "Influence easily like never before",
+                      press: () {},
+                    ),
+                    ChapterCard(
+                      name: "Win",
+                      chapterNumber: 1,
+                      tag: "Wining is what matters",
+                      press: () {},
+                    ),
+                    SizedBox(height: 10,)
+                  ],
+                ),
+              )
+            ],
+          )
         ],
       ),
+    );
+  }
+}
+
+class ChapterCard extends StatelessWidget {
+  final String name;
+  final String tag;
+  final int chapterNumber;
+  final VoidCallback press;
+
+  const ChapterCard({
+    super.key,
+    required this.name,
+    required this.tag,
+    required this.chapterNumber,
+    required this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+      margin: EdgeInsets.only(bottom: 16),
+      width: size.width - 48,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(38.5),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 33,
+            color: Color(0xFFD3D3D3).withOpacity(.84),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "Chapter $chapterNumber : $name \n",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: kBlackColor,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: tag,
+                  style: TextStyle(color: kLightBlackColor),
+                )
+              ],
+            ),
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: press,
+            iconSize: 18,
+            icon: Icon(
+              Icons.arrow_forward_ios,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BookInfo extends StatelessWidget {
+  const BookInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Crushing &",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                "Influence",
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "When the earth was flat and everyone wanted of the best and winning with an A game with all. the things you have",
+                          maxLines: 5,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: kLightBlackColor,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        RoundedButton(
+                          text: "Read",
+                          press: () {},
+                          verticlePadding: 10,
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border),
+                      ),
+                      BookRating(score: 4.9)
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        Image.asset(
+          "assets/images/book-1.png",
+          height: 200,
+        )
+      ],
     );
   }
 }
