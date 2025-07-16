4bea6d17a3df571d87c49206ea81f3c1a4e6ffd7
brianmujjuni <brianmujjuni@engineer.com>
2025-07-16 21:14:06 +0300

    fialize details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -11,70 +11,170 @@ class DetailsScreen extends StatelessWidget {
     var size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              Container(
-                height: size.height * .4,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/bg.png"),
-                    fit: BoxFit.fitWidth,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Container(
+                  height: size.height * .4,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/bg.png"),
+                      fit: BoxFit.fitWidth,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
                   ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: size.height * .1,
+                        ),
+                        BookInfo()
+                      ],
+                    ),
                   ),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24),
+                Padding(
+                  padding: EdgeInsets.only(top: size.height * .4 - 30),
                   child: Column(
                     children: [
-                      SizedBox(
-                        height: size.height * .1,
+                      ChapterCard(
+                        name: "Money",
+                        chapterNumber: 1,
+                        tag: "Life is about change",
+                        press: () {},
                       ),
-                      BookInfo()
+                      ChapterCard(
+                        name: "Power",
+                        chapterNumber: 1,
+                        tag: "Everything loves. power",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Influence",
+                        chapterNumber: 1,
+                        tag: "Influence easily like never before",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Win",
+                        chapterNumber: 1,
+                        tag: "Wining is what matters",
+                        press: () {},
+                      ),
+                      SizedBox(
+                        height: 10,
+                      )
                     ],
                   ),
-                ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.headlineSmall,
+                      children: [
+                        TextSpan(text: "You might also "),
+                        TextSpan(
+                          text: "Like...",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        height: 180,
+                        width: double.infinity,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          padding:
+                              EdgeInsets.only(left: 24, top: 24, right: 150),
+                          height: 200,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(29),
+                            color: Color(0xFFFFF8F9),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(color: kBlackColor),
+                                  children: [
+                                    TextSpan(
+                                      text:
+                                          "How To Win \nFriends & Influence \n",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "Automex Technologies",
+                                      style: TextStyle(color: kLightBlackColor),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  BookRating(score: 4.9),
+                                  SizedBox(width: 20,),
+                                  Expanded(
+                                      child: RoundedButton(
+                                    text: "Read",
+                                    press: () {},
+                                    verticlePadding: 10,
+                                  ))
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Image.asset(
+                          "assets/images/book-3.png",
+                          width: 150,
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.only(top: size.height * .4 - 30),
-                child: Column(
-                  children: [
-                    ChapterCard(
-                      name: "Money",
-                      chapterNumber: 1,
-                      tag: "Life is about change",
-                      press: () {},
-                    ),
-                    ChapterCard(
-                      name: "Power",
-                      chapterNumber: 1,
-                      tag: "Everything loves. power",
-                      press: () {},
-                    ),
-                    ChapterCard(
-                      name: "Influence",
-                      chapterNumber: 1,
-                      tag: "Influence easily like never before",
-                      press: () {},
-                    ),
-                    ChapterCard(
-                      name: "Win",
-                      chapterNumber: 1,
-                      tag: "Wining is what matters",
-                      press: () {},
-                    ),
-                    SizedBox(height: 10,)
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
+            ),
+            SizedBox(
+              height: 40,
+            )
+          ],
+        ),
       ),
     );
   }
